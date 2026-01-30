14ed1be0ccddf0119b30e1c51c821fc01c025a1e
Denis Lusson <denis.lusson@gmail.com>
2026-01-30 10:21:56 -0800

    Relax openai version

--- marker/converters/pdf.py
+++ marker/converters/pdf.py
@@ -46,7 +46,6 @@ from marker.schema.registry import register_block_class
 from marker.util import strings_to_classes
 from marker.processors.llm.llm_handwriting import LLMHandwritingProcessor
 from marker.processors.order import OrderProcessor
-from marker.services.gemini import GoogleGeminiService
 from marker.processors.line_merge import LineMergeProcessor
 from marker.processors.llm.llm_mathblock import LLMMathBlockProcessor
 from marker.processors.llm.llm_page_correction import LLMPageCorrectionProcessor
@@ -99,7 +98,7 @@ class PdfConverter(BaseConverter):
         BlankPageProcessor,
         DebugProcessor,
     )
-    default_llm_service: BaseService = GoogleGeminiService
+    default_llm_service: BaseService = BaseService
 
     def __init__(
         self,
